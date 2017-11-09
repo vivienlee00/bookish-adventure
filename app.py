9bf7bd9f1d4701bb10c2c46cd789bc5f843e428d
vivienlee00 <vivien.lee.601@gmail.com>
2017-11-09 12:13:58 -0500

    IT WORKS

--- app.py
+++ app.py
@@ -3,25 +3,20 @@
 #HW4 -- Fill Up Yer Flask
 #2017-09-20
 
-from flask import Flask
+from flask import Flask, render_template
+import urllib2
+import json
 
 app = Flask("__name__")
 
+
+
 @app.route("/")
 def hello_world():
-    return "hello"
-
-@app.route("/pg2")
-def hello_world2():
-    return "hello2"
-
-@app.route("/pgCat")
-def hello_worldCat():
-    return "<img src=\"http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg\">"
-
-@app.route("/pgBackpack")
-def hello_worldBackpack():
-    return "<img src=\"https://media.gucci.com/style/DarkGray_Center_0_0_800x800/1478799009/431570_CAO0G_1000_002_086_0000_Light-Soho-leather-chain-backpack.jpg\">"
+    siteurl = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=TUVnwOk0lvflM6FqvNJRUzyuVcHTN1ciFHPip9dU")
+    siteurl2= siteurl.read()
+    elements = json.loads(siteurl2)
+    return render_template('temp.html', img = elements["url"])
 
 if __name__ == "__main__":
     app.debug = True
