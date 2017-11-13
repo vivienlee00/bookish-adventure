573b05593a94533f1dacb05c027bbc153ab483d2
vivienlee00 <vivien.lee.601@gmail.com>
2017-11-12 22:15:12 -0500

    done

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ def hello_world():
     elements = json.loads(siteurl2)
     return render_template('temp.html', img = elements["url"], exp = elements["explanation"])
 
+@app.route("/flickr")
+def test_flickr():
+    siteurl = urllib2.urlopen("https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0163f6e2944d794716ea1001a8149b7d&photo_id=22235517575&format=json")
+    siteurl2= siteurl.read()
+    siteurl3= siteurl2[14:-1]
+    elements = json.loads(siteurl3)
+    farmid = str(elements['photo']['farm'])
+    serverid = str(elements['photo']['server'])
+    photoid = str(elements['photo']['id'])
+    secretid = str(elements['photo']['secret'])
+    formid = str(elements['photo']['originalformat'])
+    imgurl = 'https://farm' + farmid + '.staticflickr.com/' + serverid + '/'+ photoid + '_' + secretid + '.' + formid
+    author = 'Photo by ' + elements['photo']['owner']['realname']
+    return render_template('temp.html', img = imgurl, exp = author)
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
